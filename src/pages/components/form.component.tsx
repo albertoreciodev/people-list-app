a74e3d7c985de3cca0c43760b9ab65cff5bb3906
Alberto Recio <albertoreciodev@gmail.com>
2024-01-22 23:07:18 +0100

    Fix some forms code

--- src/pages/components/form.component.tsx
+++ src/pages/components/form.component.tsx
@@ -1,14 +1,16 @@
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material/";
 import FormGroup from "@mui/material/FormGroup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialData, initialDataProps } from "@/initial-data";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
 export const Form = () => {
+  // Valores de la API - Que vienen del fichero initial data
   const [dataForm, setDataForm] = useState<initialDataProps[]>(initialData);
-  const [formValuesDefault, setFormValuesDefault] = useState({
+  // Valores del form que capturo
+  const [formValuesUser, setFormValuesUser] = useState({
     name: "",
     lastName: "",
     jobTitle: "",
@@ -25,72 +27,82 @@ export const Form = () => {
 
     const submitDataForm = {
       id: nextDataFormId,
-      name: event.target.name.value,
-      lastName: event.target.lastName.value,
-      jobTitle: event.target.jobTitle.value,
-      birthDate: event.target.birthDate.value,
-      active: event.target.active.checked,
+      name: event.currentTarget.name.value,
+      lastName: event.currentTarget.lastName.value,
+      jobTitle: event.currentTarget.jobTitle.value,
+      birthDate: event.currentTarget.birthDate.value,
+      active: event.currentTarget.active.checked,
     };
+
+    setFormValuesUser(submitDataForm);
     setDataForm([...dataForm, submitDataForm]);
 
     // Reset form
-    const emptyValues = {
+    const emptyFormValuesUser = {
       name: "",
       lastName: "",
       jobTitle: "",
       birthDate: "",
       active: false,
     };
-    setFormValuesDefault(emptyValues);
+    setFormValuesUser(emptyFormValuesUser);
   };
+  useEffect(() => {
+    alert("useEffect");
+  }, [dataForm]);
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ display: "flex", flexDirection: "row", gap: "10px" }}
-    >
-      <TextField
-        id="name"
-        label="Name"
-        type="text"
-        variant="outlined"
-        defaultValue={formValuesDefault.name}
-      />
-      <TextField
-        id="lastName"
-        label="Last name"
-        type="text"
-        variant="outlined"
-        defaultValue={formValuesDefault.lastName}
-      />
-      <TextField
-        id="jobTitle"
-        label="Job title"
-        type="text"
-        variant="outlined"
-        defaultValue={formValuesDefault.jobTitle}
-      />
-      <TextField
-        id="birthDate"
-        //label="Birth date"
-        type="date"
-        variant="outlined"
-        defaultValue={formValuesDefault.birthDate}
-      />
-
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox name="active" defaultValue={undefined} />}
-          labelPlacement="start"
-          label="Active"
+    <>
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "row", gap: "10px" }}
+      >
+        <TextField
+          id="name"
+          label="Name"
+          type="text"
+          variant="outlined"
+          defaultValue={formValuesUser.name}
         />
-      </FormGroup>
 
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-      >{`Create new user`}</Button>
-    </form>
+        <TextField
+          id="lastName"
+          label="Last name"
+          type="text"
+          variant="outlined"
+          defaultValue={formValuesUser.lastName}
+        />
+        <TextField
+          id="jobTitle"
+          label="Job title"
+          type="text"
+          variant="outlined"
+          defaultValue={formValuesUser.jobTitle}
+        />
+        <TextField
+          id="birthDate"
+          //label="Birth date"
+          type="date"
+          variant="outlined"
+          defaultValue={formValuesUser.birthDate}
+        />
+
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox name="active" defaultValue={undefined} />}
+            labelPlacement="start"
+            label="Active"
+          />
+        </FormGroup>
+
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+        >{`Create new user`}</Button>
+      </form>
+      <pre>{JSON.stringify(dataForm, null, 4)}</pre>
+      <pre>{JSON.stringify(formValuesUser, null, 4)}</pre>
+    </>
   );
 };
